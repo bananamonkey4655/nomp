3e7a220f744fa63d228f5dde0e41969d21c6b4ca
Dawson <e0857426@u.nus.edu>
2022-06-30 13:37:05 +0800

    Actual_URL
    changed

--- client/src/config.js
+++ client/src/config.js
@@ -2,6 +2,6 @@ const TEST_URL = "http://localhost:8000";
 const ACTUAL_URL = "https://backend-nomp.herokuapp.com";
 
 // Change URL here
-const BACKEND_URL = TEST_URL;
+const BACKEND_URL = ACTUAL_URL;
 
 export default BACKEND_URL;
