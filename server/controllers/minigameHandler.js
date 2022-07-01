de00ccc09e032c9a068beada1eea5a9d9b603264
Humphrey <vanillaqicecream@gmail.com>
2022-07-01 13:13:33 +0800

    Refactor minigame logic

--- server/controllers/minigameHandler.js
+++ server/controllers/minigameHandler.js
@@ -2,8 +2,6 @@ const eateries = new Map();
 
 module.exports = (io) => {
   const addEateryVote = ({ eateryId, roomId }) => {
-    console.log(eateryId);
-    console.log(roomId);
     if (!eateries.has(roomId)) {
       eateries.set(roomId, new Map());
     }
@@ -14,8 +12,6 @@ module.exports = (io) => {
       const curr = voteCounts.get(eateryId);
       voteCounts.set(eateryId, curr + 1);
     }
-    console.log(voteCounts);
-    console.log("Add---------------------------------------");
   };
 
   const changeMemberDoneStatus = (name, map, roomId) => {
@@ -27,21 +23,17 @@ module.exports = (io) => {
   };
 
   const handleGameOver = (roomId) => {
-    //get the eateries with highest votes maybe top 3?
-    //send them back to the client
-    //emit new event
-    console.log(roomId);
     const voteCounts = eateries.get(roomId);
-    console.log(voteCounts);
     let max = 0;
     let highestVotedEatery = null;
+
     for (const [eatery, count] of voteCounts) {
-      if (count > max) {
+      if (count >= max) {
         max = count;
         highestVotedEatery = eatery;
       }
     }
-    console.log(`${highestVotedEatery} wins with count of ${max}`);
+
     io.in(roomId).emit("show-results", {
       eateryId: highestVotedEatery,
       count: max,
@@ -49,7 +41,6 @@ module.exports = (io) => {
   };
 
   const isGameOver = (map, roomId) => {
-    console.log("Checking if game is over...");
     const roomMembers = map.get(roomId);
     for (const member of roomMembers) {
       if (!member.done) {
